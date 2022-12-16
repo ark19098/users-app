73c65340329a7f35590a6fbf2a380d33915b7d2e
Ankur Kumar <ark19098@gmail.com>
2022-12-16 19:14:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Adduser from './components/Users/Adduser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const addUser = (user) => {
+    setUsers([...users, user]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Adduser addUserHandler={addUser} />
+      <UsersList  users={users} />
     </div>
   );
 }
